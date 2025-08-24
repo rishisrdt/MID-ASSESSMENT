6d9cf71b0e0757eb208825a065cc9827d2554e2c
rishisrdt <rishi.srdt@gmail.com>
2025-08-24 16:11:09 +0530

    Update README.md

--- src/main/java/com/assessment/mid/repository/DepartmentRepo.java
+++
@@ -1,7 +0,0 @@
-package com.assessment.mid.repository;
-
-import com.assessment.mid.entity.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartmentRepo extends JpaRepository<Department, Long> {
-}
--- src/main/java/com/assessment/mid/repository/EmployeeRepo.java
+++
@@ -1,8 +0,0 @@
-package com.assessment.mid.repository;
-
-
-import com.assessment.mid.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepo extends JpaRepository<Employee, Long> {
-}
